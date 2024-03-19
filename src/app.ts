9626b28aa1a9d23c2bc99754c9f9b1c2e3cc6357
Alvaro Bernal <alvaro-122010@hotmail.com>
2024-03-19 15:10:07 -0500

    Add new dependencies and configure CORS and body-parser

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,28 @@
-import ExpressServer from './server/expres-server';
+import cors from "cors";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import ExpressServer from "./server/expres-server";
 
-const HOST: string = process.env.HOST || 'localhost';
+const HOST: string = process.env.HOST || "localhost";
 const PORT: number = Number(process.env.POR) || 3000;
 
 const server = new ExpressServer(HOST, PORT);
+
+// search for the environment file according to the environment in which the application is running
+const pathEnvirontment = process.env.NODE_ENV
+  ? `.env.${process.env.NODE_ENV}`
+  : ".env";
+
+// Loading environment variables
+dotenv.config({
+  path: pathEnvirontment,
+});
+
+// CORS configuration
+server.getExpress().use(cors());
+
+// pody-parser configuration
+server.getExpress().use(bodyParser.json());
 
 try {
   server.listen();
