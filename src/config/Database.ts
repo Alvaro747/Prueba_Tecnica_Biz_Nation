c9c1ed34e84e58b4b87f4bc9c80057519e357f83
Alvaro Bernal <alvaro-122010@hotmail.com>
2024-03-19 23:53:56 -0500

    Refactor Database class to use consistent quotation marks and improve code formatting

--- src/config/Database.ts
+++ src/config/Database.ts
@@ -1,25 +1,35 @@
-import {Sequelize} from 'sequelize';
-import IDatabase from '@/interfaces/database/database.interface';
-import ConfigEnviroments from './database-enviroments.config';
+import {Sequelize} from "sequelize";
+import IDatabase from "@/interfaces/database/database.interface";
+import ConfigEnviroments from "./database-enviroments.config";
 
 class Database {
-    private static instance: Database;
+  private static instance: Database;
+  private sequelize: Sequelize;
 
-    private constructor(config: IDatabase) {
-        new Sequelize(config.dbName, config.dbUser, config.dbPassword, {
-            host: config.dbHost,
-            dialect: config.dbDialect,
-            port: config.dbPort,
-        });
-    }
+  private constructor(config: IDatabase) {
+    this.sequelize = new Sequelize(
+      config.dbName,
+      config.dbUser,
+      config.dbPassword,
+      {
+        host: config.dbHost,
+        dialect: config.dbDialect,
+        port: config.dbPort,
+      }
+    );
+  }
 
-    public static getInstance(): Database {
-        const configEnviroment = ConfigEnviroments.getInstance().getConfig();
-        if (!Database.instance) {
-            Database.instance = new Database(configEnviroment);
-        }
-        return Database.instance;
+  public static getInstance(): Database {
+    const configEnviroment = ConfigEnviroments.getInstance().getConfig();
+    if (!Database.instance) {
+      Database.instance = new Database(configEnviroment);
     }
+    return Database.instance;
+  }
+
+  public getSequelize(): Sequelize {
+    return this.sequelize;
+}
 }
 
 export default Database;
