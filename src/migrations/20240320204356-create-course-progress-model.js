7170da1e29a156a76bc34dceca58e394f02b81c2
Alvaro Bernal <alvaro-122010@hotmail.com>
2024-03-20 16:32:24 -0500

    Add migration for creating CourseProgressModel table

---
+++ src/migrations/20240320204356-create-course-progress-model.js
@@ -0,0 +1,54 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("CourseProgressModels", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      status: {
+        type: Sequelize.ENUM("pending", "in_progress", "completed"),
+        allowNull: false,
+        defaultValue: "pending",
+      },
+      approvalDate: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "UserModels",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      courseId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "CourseModels",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("CourseProgressModels");
+  },
+};
