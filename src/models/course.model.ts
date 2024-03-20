6039aba615f5cd4371c22b2b0ec89caba5a9e74d
Alvaro Bernal <alvaro-122010@hotmail.com>
2024-03-19 23:56:45 -0500

    Add CourseModel to src/models/course.model.ts

---
+++ src/models/course.model.ts
@@ -0,0 +1,25 @@
+import { DataTypes, Model } from "sequelize";
+import Database from "@/config/Database";
+/* import CourseProgress from "./CourseProgress"; */
+
+// Get sequelize from singleton instance from Database
+const sequelize = Database.getInstance().getSequelize();
+
+// Define the CourseModel model
+class CourseModel extends Model {}
+
+CourseModel.init(
+  {
+    logo: DataTypes.STRING,
+    title: DataTypes.STRING,
+    description: DataTypes.TEXT,
+    publicationDate: DataTypes.DATE,
+    introductoryVideo: DataTypes.STRING,
+  },
+  { sequelize, modelName: "course" }
+);
+
+/* // Define relationships
+CourseModel.hasMany(CourseProgress); */
+
+module.exports = CourseModel;
