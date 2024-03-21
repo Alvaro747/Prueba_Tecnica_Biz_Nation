e9f6895b72790038d5a70d62e2d30e3e5aa7783e
Alvaro Bernal <alvaro-122010@hotmail.com>
2024-03-20 22:48:46 -0500

    Fix formatting in AuthController login method

--- src/controllers/auth/auth.controller.ts
+++ src/controllers/auth/auth.controller.ts
@@ -1,10 +1,11 @@
 import {Request, Response} from "express";
+
 import {AuthService} from "../../services/index";
 
 export default class AuthController {
   static async login(req: Request, res: Response) {
     try {
-      const response = await AuthService.login(req.body );
+      const response = await AuthService.login(req.body);
       res.status(response?.status || 201).json(response);
     } catch (error) {
       res.status(500).json({error: "Internal Server Error"});
