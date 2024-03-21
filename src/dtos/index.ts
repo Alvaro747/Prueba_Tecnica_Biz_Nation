4921341ea73ad211a361dd648c4ee0b3c5485d1a
Alvaro Bernal <alvaro-122010@hotmail.com>
2024-03-20 22:48:51 -0500

    Add RegistrationResponseDto to dtos index

--- src/dtos/index.ts
+++ src/dtos/index.ts
@@ -1,4 +1,5 @@
 import UserRegisterDto from "./auth/register-user.dto";
 import UserLoginDto from "./auth/login-user.dto";
+import RegistrationResponseDto from "./auth/response-register.dto";
 
-export {UserRegisterDto, UserLoginDto};
+export {UserRegisterDto, UserLoginDto, RegistrationResponseDto};
