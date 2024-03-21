a322ed04d7fd417f492a6ce5e3f9e187bd37e9e0
Alvaro Bernal <alvaro-122010@hotmail.com>
2024-03-21 10:47:39 -0500

    Add new middlewares for authentication and user role verification

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,3 +1,11 @@
 import validationBodyMiddleware from "./validations/validation-body.middleware";
+import verifyTokenMiddleware from "./auth/verify-token.middleware";
+import getUserRoleByEmailMiddleware from "./auth/get-user-role.middleware";
+import VerifyUserRoleMiddleware from "./auth/verify-role.middleware";
 
-export {validationBodyMiddleware};
+export {
+  validationBodyMiddleware,
+  verifyTokenMiddleware,
+  getUserRoleByEmailMiddleware,
+  VerifyUserRoleMiddleware,
+};
