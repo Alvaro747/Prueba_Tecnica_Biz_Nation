985dee92de6755ac5782ad53f516b1311a9bae36
Alvaro Bernal <alvaro-122010@hotmail.com>
2024-03-20 21:09:33 -0500

    Add CourseModel to src/models

---
+++ src/models/coursemodel.ts
@@ -0,0 +1,46 @@
+import { ICourseModel } from "../interfaces/index";
+import {Model, Sequelize} from "sequelize";
+
+
+export default (sequelize: Sequelize, DataTypes: any) => {
+  class CourseModel
+    extends Model<ICourseModel>
+    implements ICourseModel
+  {
+    public logo!: string;
+    public title!: string;
+    public description!: string;
+    public publicationDate!: Date;
+    public introductoryVideo!: string;
+
+    static associate(models: any): void {
+      // Relación uno a muchos con LessonModel
+      this.hasMany(models.LessonModel, {
+        foreignKey: "courseId",
+        as: "lessons", // Nombre opcional para la asociación
+      });
+
+      // Relación uno a muchos con CourseProgressModel
+      this.hasMany(models.CourseProgressModel, {
+        foreignKey: "courseId",
+        as: "courseProgresses", // Nombre opcional para la asociación
+      });
+    }
+  }
+
+  CourseModel.init(
+    {
+      logo: DataTypes.STRING,
+      title: DataTypes.STRING,
+      description: DataTypes.TEXT,
+      publicationDate: DataTypes.DATE,
+      introductoryVideo: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "CourseModel",
+    }
+  );
+
+  return CourseModel;
+};
