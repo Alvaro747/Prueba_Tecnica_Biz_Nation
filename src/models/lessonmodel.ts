6f1f202e5ad2c53a61a7034d41abdf633afb84bd
Alvaro Bernal <alvaro-122010@hotmail.com>
2024-03-20 21:09:23 -0500

    Add LessonModel class definition

---
+++ src/models/lessonmodel.ts
@@ -0,0 +1,40 @@
+import {ILessonModel} from "@/interfaces";
+import {Model, Sequelize} from "sequelize";
+
+export default (sequelize: Sequelize, DataTypes: any) => {
+  class LessonModel extends Model<ILessonModel> implements ILessonModel {
+    public title!: string;
+    public description!: string;
+    public video!: string;
+    public courseId!: number;
+
+    static associate(models: any): void {
+      // Many-to-one relationship with CourseModel
+      this.belongsTo(models.CourseModel, {
+        foreignKey: "courseId",
+        as: "course", // Optional name for the association
+      });
+
+      // One-to-many relationship with LessonProgressModel
+      this.hasMany(models.LessonProgressModel, {
+        foreignKey: "lessonId",
+        as: "lessonProgresses", // Optional name for the association
+      });
+    }
+  }
+
+  LessonModel.init(
+    {
+      title: DataTypes.STRING,
+      description: DataTypes.TEXT,
+      video: DataTypes.STRING,
+      courseId: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "LessonModel",
+    }
+  );
+
+  return LessonModel;
+};
