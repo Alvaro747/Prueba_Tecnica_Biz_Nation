e092f8245eeecb3a69e600099db3cca4f7a7a2b1
Alvaro Bernal <alvaro-122010@hotmail.com>
2024-03-20 22:49:07 -0500

    Add allowNull and unique constraints to email field

--- src/migrations/20240320200642-create-user-model.js
+++ src/migrations/20240320200642-create-user-model.js
@@ -16,7 +16,9 @@ module.exports = {
         type: Sequelize.DATE
       },
       email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
       },
       password: {
         type: Sequelize.STRING
