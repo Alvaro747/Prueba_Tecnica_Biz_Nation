c53a5dc2c26f035579269a25e4b275ae86c95d00
Alvaro Bernal <alvaro-122010@hotmail.com>
2024-03-20 22:54:07 -0500

    Refactor auth controller login and register methods

--- src/controllers/auth/auth.controller.ts
+++ src/controllers/auth/auth.controller.ts
@@ -4,20 +4,12 @@ import {AuthService} from "../../services/index";
 
 export default class AuthController {
   static async login(req: Request, res: Response) {
-    try {
-      const response = await AuthService.login(req.body);
-      res.status(response?.status || 201).json(response);
-    } catch (error) {
-      res.status(500).json({error: "Internal Server Error"});
-    }
+    const response = await AuthService.login(req.body);
+    res.status(response?.status || 201).json(response);
   }
 
   static async register(req: Request, res: Response) {
-    try {
-      const response = await AuthService.register(req.body);
-      res.status(response?.status || 201).json(response);
-    } catch (error) {
-      res.status(500).json({error: "Internal Server Error"});
-    }
+    const response = await AuthService.register(req.body);
+    res.status(response?.status || 201).json(response);
   }
 }
